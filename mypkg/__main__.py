ed832a15957d4e636bccdc8c305d76e0e6ba65a0
Ludovico Bianchi <lbianchi@lbl.gov>
2022-02-27 12:51:58 -0600

    Display runtim info from setuptools_scm.get_version()

--- mypkg/__main__.py
+++ mypkg/__main__.py
@@ -1,4 +1,6 @@
 from . import __version__
+from setuptools_scm import get_version
 
 
 print(f"{__version__=}")
+print(f"{get_version()=}")
--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="mypkgdistr",
     setup_requires=["setuptools_scm"],
+    install_requires=["setuptools_scm"],
     use_scm_version=True
 )
