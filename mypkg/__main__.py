9cb400870de10930d76bc3d55fcd7452769ca346
Ludovico Bianchi <lbianchi@lbl.gov>
2022-02-27 13:05:39 -0600

    Try different variants

--- mypkg/__main__.py
+++ mypkg/__main__.py
@@ -1,6 +1,22 @@
-from . import __version__
-from setuptools_scm import get_version
+from typing import List, Dict
 
+from setuptools_scm import get_version
+from . import __version__
+
+
+def get_versions(variants: List[Dict]):
+    return {
+        get_version(**kwargs): kwargs
+        for kwargs in variants
+    }
+
+
+setuptools_scm_variants = get_versions(
+    [
+        {},
+        dict(version_scheme="post-release"),
+    ]
+)
 
 print(f"{__version__=}")
-print(f"{get_version()=}")
+print(f"{setuptools_scm_variants=}")
--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name="mypkgdistr",
     setup_requires=["setuptools_scm"],
     install_requires=["setuptools_scm"],
-    use_scm_version=True
+    use_scm_version={
+        "version_scheme": "post-release",
+    }
 )
