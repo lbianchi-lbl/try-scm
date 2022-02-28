9f04b2e951014d5d18651eeba8e3d0e90760bef2
Ludovico Bianchi <lbianchi@lbl.gov>
2022-02-28 14:54:23 -0600

    Handle cases where setuptools_scm cannot be used

--- mypkg/__main__.py
+++ mypkg/__main__.py
@@ -1,14 +1,22 @@
+import logging
 from typing import List, Dict
 
 from setuptools_scm import get_version
 from . import __version__
 
 
+_logger = logging("mypkg")
+
+
 def get_versions(variants: List[Dict]):
-    return {
-        get_version(**kwargs): kwargs
-        for kwargs in variants
-    }
+    versions = {}
+    try:
+        key = get_version(**kwargs)
+    except LookupError:
+        _logger.exception("Version could not be determined using setuptools_scm")
+    else:
+        versions[key] = kwargs
+    return versions
 
 
 setuptools_scm_variants = get_versions(
