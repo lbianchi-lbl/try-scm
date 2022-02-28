7a68462aaa340ef14cfc3339db4ebe7d07ee060b
Ludovico Bianchi <lbianchi@lbl.gov>
2022-02-28 14:26:35 -0600

    Enable installing with setuptools_scm from GitHub archive

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="mypkgdistr",
-    setup_requires=["setuptools_scm"],
+    setup_requires=[
+        "setuptools_scm",
+        "setuptools_scm_git_archive",
+    ],
     install_requires=["setuptools_scm"],
     use_scm_version={
         "version_scheme": "post-release",
