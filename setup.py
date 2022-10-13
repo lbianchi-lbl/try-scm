0153b48172842381725e3acc0213abc441870e28
Ludovico Bianchi <lbianchi@lbl.gov>
2022-10-13 09:54:49 -0500

    Tweak local scheme

--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
 from setuptools import setup, find_packages
 
+
+def _get_scm_version_config(
+        time_format=r"%Y%m%d%H%M%S",
+        local_sep: str = ".",
+    ):
+    def _verbatim_tag(v):
+        return str(v.tag)
+
+    def _distance_node_timestamp(v):
+        if v.exact:
+            return ""
+        parts = [
+            "{distance}",
+            "{node}",
+        ]
+        if v.dirty:
+            parts.append(f"d{{time:{time_format}}}")
+        fmt = "+" + local_sep.join(parts)
+        return v.format_with(fmt)
+
+
+    return {
+        "version_scheme": _verbatim_tag,
+        "local_scheme": _distance_node_timestamp,
+    }
+
 setup(
     name="mypkgdistr",
     packages=find_packages(),
     setup_requires=[
         "setuptools_scm>=7",
     ],
-    use_scm_version={
-        "version_scheme": lambda v: str(v.tag),
-        "local_scheme": "node-and-timestamp",
-    }
+    use_scm_version=_get_scm_version_config,
 )
